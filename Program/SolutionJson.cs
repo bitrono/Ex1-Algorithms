dae5deb9d9009b69808159bf60ee965663400657
Ofir Bitron <bitrono@yahoo.com>
2017-04-17 09:29:53 +0300

    Fixed bfs algorithm and added ToJson function for adapter

---
+++ Program/SolutionJson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeAdapter
+{
+    public class SolutionJson
+    {
+        public string name { get; set; }
+        public string solution { get; set; }
+        public string evalNodes { get; set; }
+
+        public SolutionJson(string name, string solution, string evalNodes)
+        {
+            this.name = name;
+            this.solution = solution;
+            this.evalNodes = evalNodes;
+        }
+    }
+}
